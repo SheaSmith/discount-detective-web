49f7f64e5c99609a3f1d59167e589f799fe7330b
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2022-09-05 23:10:59 +1200

    Possible fix for entity manager missing

--- api/src/main/kotlin/nz/shea/discountdetective/api/ScraperTask.kt
+++ api/src/main/kotlin/nz/shea/discountdetective/api/ScraperTask.kt
@@ -10,6 +10,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 import javax.persistence.EntityManager
 
 @Component
@@ -19,6 +20,7 @@ class ScraperTask(
     private val entityManager: EntityManager
 ) {
 
+    @Transactional
     @Scheduled(cron = "0 2 * * *")
     @EventListener(ApplicationReadyEvent::class)
     fun runScrapers() {
